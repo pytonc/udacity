03a53154bce5409bba035947fdfe7549d651a502
udacians <pytonc@gmail.com>
2012-07-05 01:23:34 +0300

    making it runnable

--- classic-games/hello_class/game.py
+++ classic-games/hello_class/game.py
@@ -44,3 +44,37 @@ class Archer(Character):
         if abs(enemy.x - self.x) <= 5 and (enemy.y == self.y):
             enemy.hp -= 5
 
+if __name__ == '__main__':
+    #initializing some entities
+    student = Character(10, 10, 100)
+    bug1 = Character(12, 10, 100)
+    print """Welcome to 'Hello, Class' game
+    Available commands are:
+    r - move right
+    l - move left
+    a - attack
+    gps - print location
+    
+    There is a Bug 2 steps to the right from you.
+    You should probably do something about it!
+    """
+
+    while True:
+        c = raw_input("You > ")
+        if c == "exit":
+            break
+        elif c == "r":
+            student.moveRight()
+            print "You now are at location: ", student.x, student.y
+        elif c =="l":
+            student.moveLeft()
+        elif c == "gps":
+            print "Your GPS location: ", student.x, student.y
+            print "Bug GPS location: ", bug1.x, bug1.y
+        elif c == "a":
+            student.attack(bug1)
+            print "Bug now has: ", bug1.hp, " hp left"
+        else:
+            print "Unknown command. 'exit' to exit the game"
+
+
