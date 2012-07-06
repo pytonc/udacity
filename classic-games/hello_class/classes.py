67dedfee8362b29242e2763178b6919032e008be
udacians <pytonc@gmail.com>
2012-07-07 00:34:21 +0300

    split class definitions into a separate file and added README

---
+++ classic-games/hello_class/classes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# game.py - simple game to demonstrate classes and objects
+
+world = [[None for x in range(100)] for y in range(100)]
+
+class Entity:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        world[x][y] = self
+    
+    def remove(self):
+        world[self.x][self.y] = None
+
+class Character(Entity):
+    def __init__(self, x, y, hp):
+        Entity.__init__(self, x, y)
+        self.hp = hp
+        self.items = []
+    
+    def moveLeft(self):
+        self.x -= 1
+    
+    def moveRight(self):
+        self.x += 1
+
+    def attack(self, enemy):
+        if abs(enemy.x - self.x) == 1 and (enemy.y == self.y):
+            enemy.hp -= 10
+
+class Wizard(Character):
+    def __init__(self, x, y, hp):
+        Character.__init__(self, x, y, hp)
+    
+    def castSpell(self, enemy):
+        if abs(enemy.x - self.x) == 1 and (enemy.y == self.y):
+            enemy.remove()
+
+class Archer(Character):
+    def __init__(self, x, y, hp):
+        Character.__init__(self, x, y, hp)
+    
+    def rangeAttack(self, enemy):
+        if abs(enemy.x - self.x) <= 5 and (enemy.y == self.y):
+            enemy.hp -= 5
+
