142d6b910cf0f074ab91080cf9a70f06d492c65b
Lucia Maglione <zitron81@gmail.com>
2012-07-14 17:23:11 +0200

    Fixes a bug where width and height are inverted.

--- classic-games/hello_class/classes.py
+++ classic-games/hello_class/classes.py
@@ -4,7 +4,7 @@ class WorldMap(object):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.map = [[None for x in range(self.width)] for y in range(self.height)]
+        self.map = [[None for y in range(self.height)] for x in range(self.width)]
 
     def is_occupied(self, x, y):
         ''' Checks if a given space on the map and returns True if occupied. '''
