84990255dc45eb4e0f27f7e4d1250e0e63ff52c2
Sergio Visinoni <sergio.visinoni@blocket.se>
2012-07-15 15:38:36 +0200

    Initial class structure
    Start laying down the class hierarchy for a generic UserInterface
    and a direct implementation using PyGame

---
+++ classic-games/hello_class/ui_pygame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os, sys
+from ui import UserInterface
+
+# This class defines the Pygame (SDL) UI frontend
+
+class SDLUserInterface(UserInterface):
+    def __init__(self, width, height):
+        pass
+
+
+# vim: expandtab tabstop=4 shiftwidth=4 softtabstop=4
