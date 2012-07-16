c73e1af594c0010b8cae837bf1b872b258a66057
MikeGoldberg <gdimike@gmail.com>
2012-07-16 14:42:18 -0500

    Modified to use Pygame.

--- classic-games/hello_class/game.py
+++ classic-games/hello_class/game.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # game.py - simple game to demonstrate classes and objects
+
+# Hacked by MikeGoldberg to use Pygame
+# Mon Jul 16 12:51:31 CDT 2012
+
 from classes import *
-        
+import pygame
+import os
+
 DIRECTIONS = {
     "r": "right",
     "l": "left",
     "d": "down",
     "u": "up"
 }
+
+# Pygame (SDL) constants
+XRES = 640
+YRES = 480
+BCOLOR = (255, 255, 255)
+BLOCK_SIZE=10 # must match value in other file! This has to be bad.
+
+# Pygame: update the graphical screen
+def update_screen(players, screen):
+    screen.blit(background, (0, 0))
+    for player in players:
+        player.screen.blit(player.sprite, (10*player.x, (22*21 - 22*player.y)))
+    pygame.display.flip()
 
 if __name__ == '__main__':
 
@@ -20,41 +39,60 @@ if __name__ == '__main__':
     a - attack
     gps - print location
     x - exit
-    
+
     There is a Bug 2 steps to the right from you.
     You should probably do something about it!
     """
 
     # initializing some entities
+    players = [] 
+
+    # Pygame: set up
+    pygame.display.init()
+    os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
+    screen = pygame.display.set_mode((XRES, YRES))
+    pygame.display.set_caption('Udacity CS101 Game')
+    pygame.mouse.set_visible(1)
+
+    # Pygame: Create The Backgound
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(BCOLOR)
+
+    #Pygame: Display The Background
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
     #campus = World(100, 100)
-    student = Player(10, 10, 100)
-    engineer = Wizard(35, 14, 100)
-    bug1 = Enemy(12, 10, 100)
-    bug2 = Enemy(11, 11, 100)
-    
+    student = Player(10, 10, screen, 100)
+    players.append(student)
+    engineer = Wizard(35, 14, screen, 100)
+    players.append(engineer)
+    bug1 = Enemy(12, 10, screen, 100)
+    players.append(bug1)
+    pygame.display.flip()
+
     statusbar.set_character(student)
     world.print_map()
 
     while True:
         c = raw_input("You > ")
-        
+
         if c == "x":
             break
         elif c in DIRECTIONS:
             student.move(DIRECTIONS[c])
             bug1.act(student, DIRECTIONS)
+            update_screen(players, screen)
         elif c == "gps":
             statusbar.set_status("Your GPS location: %i %i" % (student.x, student.y))
             statusbar.set_status("Bug GPS location: %i %i" % (bug1.x, bug1.y))
         elif c == "a":
-            enemies = student.get_alive_enemies(1)
-            if enemies:
-                student.attack(enemies[0])
-                enemies[0].act(student, DIRECTIONS)
+            student.attack(bug1)
+            bug1.act(student, DIRECTIONS)
         else:
             statusbar.set_status("Unknown command. 'x' to exit game")
-            
+
         statusbar.show()
         world.print_map()
 
